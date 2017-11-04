cce734e675db6bcd6bd3e31721ee01f74cb590b0
Thorambar <thomaspach94@googlemail.com>
2017-11-04 15:55:10 +0100

    Calculation of PWM done, TODO divBy0

--- blatt_1/main.py
+++ blatt_1/main.py
@@ -1,7 +1,7 @@
 # Program to find start codons with Position weight matrices (PWM) in the TIS
 # File with a line length of 200.
 # The beginning of the start codon is at position 101
-# By Thomas Pach 1.11.2017
+# 1.11.2017
 
 # Motives are ATG,GTG,TTG 
 # ------------------------------------------------------------------------
