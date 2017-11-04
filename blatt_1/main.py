83c6b041684fabe3ed34e79afda10d1375f0239e
Thorambar <thomaspach94@googlemail.com>
2017-11-04 15:50:48 +0100

    Calculation of PWM done, TODO divBy0

--- blatt_1/main.py
+++ blatt_1/main.py
@@ -3,20 +3,43 @@
 # The beginning of the start codon is at position 101
 # By Thomas Pach 1.11.2017
 
+# Motives are ATG,GTG,TTG 
+# ------------------------------------------------------------------------
+import numpy as np
+
+
 # ==== constants ======================
+startCodons =['ATG', 'GTG', 'TTG']
 sequenceFile = "TIS-Ecoli.txt"
 pwmLength = 30 
+prob = 0.25
+seqLength = 200
 r = 1
-
+nucDict = {'A': 0, 'C': 1, 'G': 2, 'T': 3, '\n': -1}
 
 
 file = open(sequenceFile)
-lineCount = 0
-for line in file:
-	lineCount += 1
-	
-print "# File stats: ", lineCount
+freqArr = np.zeros( (4,200) ) # Nucleotide freq. 0=A, 1=C, 2=G, 3=T ; columns are positions in strings
+pwmArr = np.zeros( (4,200) ) # Calculated Position weight Matrix
 
+# Create frequency array, by counting nucleotide apparence at all locations
+for line in file:
+	for idx, char in enumerate(line):
+		if( nucDict[char] >= 0 ):
+			freqArr[nucDict[char]][idx] += 1
+
+# Calculate the PWM array with log2(freq/prob)
+for rowIdx, row in enumerate(freqArr):
+	for colIdx, cell in enumerate(row):
+		pwmArr[rowIdx][colIdx] = np.log2( cell / pwmLength / prob) # TODO: Divide by 0 encountered vielleicht nicht schlimm 
+
+print pwmArr
+
+
+
+
+# === On Exit ===========
+exit(0)
 
 
 
