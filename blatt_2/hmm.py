f790342487974a5112de1319c4e79f47ed9cf1d2
Thorambar <thomaspach94@googlemail.com>
2018-01-16 16:25:47 +0100

    .

--- blatt_2/hmm.py
+++ blatt_2/hmm.py
@@ -1,3 +1,4 @@
+# Gene classification by use of Profile HMMs and Multiple alignment. 16.01.2018
 import numpy as np
 import time
 import sys
@@ -5,13 +6,14 @@ import sys
 # ==== constants ================================
 nucDict = {'A': 0, 'C': 1, 'G': 2, 'U': 3, '-': -1}
 train_file_path = 'Daten/LSU_train.fasta'
+deleteion_threshold = 0.5
+pseudo_cnt = 0.000001
 
 # ==== open file and build a list of the lines =======
 train_file = open(train_file_path)
 train_list = train_file.readlines()
 train_line_cnt = len(train_list)
 train_line_lenght = len(train_list[1]) - 1 # without the \n
-
 
 gap_count = np.zeros( (train_line_lenght) )			# matrix to store count of gaps per column 
 nucleotide_cnt = np.zeros( (train_line_lenght, 4) )	# count of nucleotides i a column
@@ -43,7 +45,7 @@ def main():
 
     insert_list.append((0.0, 0.0)) # add initial insert state
     for i in range(0, train_line_lenght):
-    	if gap_count[i]/(train_line_cnt/2) < 0.5:
+    	if gap_count[i]/(train_line_cnt/2) < deleteion_threshold:
     		match_list.append( (get_emmition(i, 1), 0.0 ) # 0.0 is a placeholder for the transition probability
     		insert_list.append( (0.0, 0.0) )
     	else:
